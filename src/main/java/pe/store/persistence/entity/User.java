47b56517be5197f7719a6e710cce77b19cfb65e2
ChugnasIvan <ivan@ad-comp.net>
2021-11-01 19:08:24 -0500

    clases nuevas

--- src/main/java/pe/store/persistence/entity/User.java
+++ src/main/java/pe/store/persistence/entity/User.java
@@ -1,2 +1,65 @@
-package pe.store.persistence.entity;public class User {
+package pe.store.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nombre_usuario")
+    private String nombreUsuario;
+
+    @Column(name = "id_persona")
+    private Long idPersona;
+
+    @Column(name = "password_usuario")
+    private String password;
+
+    @Column(name = "status_usuario")
+    private Boolean status;
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public Long getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Long idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
